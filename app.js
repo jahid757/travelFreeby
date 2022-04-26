1ff118df76ca733d7144cb56259506af009d3bf2
Jahid <jahidhasan.developer@gmail.com>
2022-04-26 19:31:08 +0600

    done !

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-"use strict"
+"use strict";
 
 // active class toggle
 
-function activeClassToggle(id1,id2){
-    const removeClassElement = document.getElementById(id1);
-    const addClassElement = document.getElementById(id2);
-    removeClassElement.classList.remove("active");
-    addClassElement.classList.add("active");
+function activeClassToggle(id1, id2) {
+  const removeClassElement = document.getElementById(id1);
+  const addClassElement = document.getElementById(id2);
+  removeClassElement.classList.remove("active");
+  addClassElement.classList.add("active");
+}
+
+function menuToggle(id) {
+  const menu = document.getElementById(id);
+  menu.classList.toggle("active");
 }
