51300227f2317b759e10e2fc30cb44d382f63bb2
Jahid <jahidhasan.developer@gmail.com>
2022-05-25 09:05:26 +0600

    chat added

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ function menuToggle(id) {
   const menu = document.getElementById(id);
   menu.classList.toggle("active");
 }
+
+function chatToggle(id, id1){
+  const chatBox = document.getElementById(id);
+  const chatIcon = document.getElementById(id1);
+  chatBox.classList.toggle("active");
+  if(chatIcon.classList.contains("fa-comment")){
+    chatIcon.classList.remove("fa-comment");
+    chatIcon.classList.add("fa-times");
+  }else{
+    chatIcon.classList.remove("fa-times");
+    chatIcon.classList.add("fa-comment");
+  }
+}
